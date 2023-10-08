70ab286b2a6e3ef5b8529a79fb423a6d878913a1
Epeltier <epeltier@gmail.com>
2023-10-08 13:37:19 -0400

    Update CompensationController.java
    formatting fix

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -11,25 +11,25 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @RestController
-    //I landed on creating a new top level resource for Compensation rather than sub-resourcing in Employee (Ex /employee/{id}/compensation)
-    //for future extensibility of the data type. Ex - querying or updating multiple employee compensations.
-    public class CompensationController {
-        private static final Logger LOG = LoggerFactory.getLogger(com.mindex.challenge.controller.CompensationController.class);
+//I landed on creating a new top level resource for Compensation rather than sub-resourcing in Employee (Ex /employee/{id}/compensation)
+//for future extensibility of the data type. Ex - querying or updating multiple employee compensations.
+public class CompensationController {
+    private static final Logger LOG = LoggerFactory.getLogger(com.mindex.challenge.controller.CompensationController.class);
 
-        @Autowired
-        private CompensationService compensationService;
+    @Autowired
+    private CompensationService compensationService;
 
-        @PostMapping("/compensation")
-        public Compensation create(@RequestBody @Valid Compensation compensation) {
-            LOG.debug("Received compensation create request for employee [{}]", compensation);
-            return compensationService.create(compensation);
-        }
+    @PostMapping("/compensation")
+    public Compensation create(@RequestBody @Valid Compensation compensation) {
+        LOG.debug("Received compensation create request for employee [{}]", compensation);
+        return compensationService.create(compensation);
+    }
 
-        @GetMapping("/compensation")
-        //query parameter ?employeeId={id} to retrieve compensation by employeeId.
-        public Compensation read(@RequestParam @NotNull String employeeId) {
-            LOG.debug("Received compensation read request for employee id [{}]", employeeId);
+    @GetMapping("/compensation")
+    //query parameter ?employeeId={id} to retrieve compensation by employeeId.
+    public Compensation read(@RequestParam @NotNull String employeeId) {
+        LOG.debug("Received compensation read request for employee id [{}]", employeeId);
 
-            return compensationService.read(employeeId);
-        }
+        return compensationService.read(employeeId);
+    }
 }
